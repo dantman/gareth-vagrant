c68149f0719cb00f02128d4142e50c210288c33d
Daniel Friesen <daniel@nadir-seen-fire.com>
2013-04-06 13:43:52 -0700

    Make the motd more useful.

--- puppet/modules/misc/manifests/init.pp
+++ puppet/modules/misc/manifests/init.pp
@@ -9,6 +9,31 @@ class misc {
 		source => 'puppet:///modules/misc/color.sh',
 	}
 
+	file { [
+		'/etc/update-motd.d/10-help-text',
+		'/etc/update-motd.d/50-landscape-sysinfo',
+		'/etc/update-motd.d/51-cloudguest'
+	]:
+		ensure => absent,
+	}
+
+	package { ['toilet', 'toilet-fonts']:
+		ensure => present,
+		before => File['/etc/update-motd.d/60-gareth-vagrant'],
+	}
+
+	file { '/etc/update-motd.d/60-gareth-vagrant':
+		ensure => present,
+		mode   => '0755',
+		source => 'puppet:///modules/misc/gareth-vagrant-motd',
+		notify => Exec['update-motd'],
+	}
+
+	exec { 'update-motd':
+		command     => 'run-parts --lsbsysinit /etc/update-motd.d > /run/motd',
+		refreshonly => true,
+	}
+
 	# Small, nifty, useful things
 	package { [ 'ack-grep', 'htop', 'curl', 'tree' ]:
 		ensure => present,
